57e6f60b3d940fd44c0520fd41ad569ac1977363
Logan Tran <loganmartintran@icloud.com>
2017-08-28 12:27:17 -0500

    remove erreneous specs for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) {User.create!(username: 'user', email: 'user@user.com', password: 'password') }
 
-  it { is_expected.to validate_presence_of(:username) }
-  it { is_expected.to validate_uniqueness_of(:username) }
-  it { is_expected.to validate_length_of(:username).is_at_least(3) }
-
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email) }
-  it { is_expected.to validate_length_of(:email).is_at_least(3) }
-
-  it { is_expected.to validate_presence_of(:password) }
-  it { is_expected.to validate_length_of(:password).is_at_least(8) }
-
   describe "attributes" do
     it "should have username, email, and password attributes" do
       expect(user).to have_attributes(username: 'user', email: 'user@user.com', password: 'password')

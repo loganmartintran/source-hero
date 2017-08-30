1938cbed9c229221abb6df21f011ec166c646c47
Logan Tran <loganmartintran@icloud.com>
2017-08-29 20:58:57 -0500

    seed data for users and topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#create 10 users
+10.times do
+  User.create!(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+users = User.all
+
+#create 20 topics
+20.times do
+  Topic.create!(
+    title: Faker::Hacker.adjective,
+    user: users.sample
+  )
+end
+
+topics = Topic.all
+
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"

e2c8453f549672eeb06bce4b063b5e849f24acb5
Logan Tran <loganmartintran@icloud.com>
2017-08-31 20:24:24 -0500

    add crud functionality w/passing specs/

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -8,8 +8,51 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new
+    @topic.title = params[:topic][:title]
+    @topic.user = current_user
+
+    if @topic.save
+      flash[:notice] = "Your topic has been saved successfully."
+      redirect_to @topic
+    else
+      flash[:alert] = "There was a problem saving your topic. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @topic = Topic.find(params[:id])
   end
+
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.title = params[:topic][:title]
+    @topic.user = current_user
+
+    if @topic.save
+      flash[:notice] = "Your topic was updated sucessfully."
+      redirect_to @topic
+    else
+      flash[:alert] = "There was a problem updating your topic. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.delete
+      flash[:notice] = "#{@topic.title} was successfully deleted."
+      redirect_to topics_path
+    else
+      flash[:alert] = "There was a problem deleting #{@topic.title}. Please try again."
+      redirect_to @topic
+    end
+  end
+
 end
